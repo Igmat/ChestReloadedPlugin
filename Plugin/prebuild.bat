8252930ac3dda567eece26148436b011404d7246
Игорь Чулинда <Igor.Chulinda@dataart.com>
2021-03-20 14:42:36 +0100

    build: move from bat to sh for unix support

--- Plugin/prebuild.bat
+++
@@ -1,8 +0,0 @@
-cd %~dp0
-cd ..
-SET STANDALONEUNITYPATH="C:\Program Files\Unity\Editor\Unity.exe"
-SET UNITYHUBEDITORPATH="C:\Program Files\Unity\Hub\Editor\2019.4.20f1\Editor\Unity.exe"
-SET UNITYPATH=%UNITYHUBEDITORPATH%
-SET CREATEPROJECTPATH="%CD%"
-
-%UNITYPATH% -batchmode -quit -projectPath %CREATEPROJECTPATH% -executeMethod Pipeline.BuildAssetBundles
