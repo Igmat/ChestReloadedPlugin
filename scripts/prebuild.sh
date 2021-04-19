1028b19f34653ca1c93f7ca68339c1dd8a3505fb
Igmat <ichulinda@hotmail.com>
2021-04-19 11:55:54 +0200

    ci: rework references to use game files

--- scripts/prebuild.sh
+++ scripts/prebuild.sh
@@ -1,12 +1,3 @@
-[ -z "$STANDALONEUNITYPATH" ] && STANDALONEUNITYPATH="/c/Program Files/Unity/Editor/Unity.exe"
-[ -z "$UNITYHUBEDITORPATH" ] && UNITYHUBEDITORPATH="/c/Program Files/Unity/Hub/Editor/2019.4.20f1/Editor/Unity.exe"
-# use next line if you installed Unity separatly (but note, that you should use 2019.4.20f1 version)
-# [ -z "$UNITYPATH" ] && UNITYPATH=STANDALONEUNITYPATH
-
-# TODO: find a way how to use declared UNITYPATH variable in GH
-# use next line if you installed Unity using Unity Hub
-UNITYPATH=$UNITYHUBEDITORPATH
-
 # get path to directory where script is located
 MY_PATH="`dirname \"$0\"`"
 # get absolute path to solution itself
@@ -17,18 +8,30 @@ if [ -z "$CREATEPROJECTPATH" ] ; then
   exit 1  # fail
 fi
 
+# read paths from config
+UNITYDIR=`LC_ALL=en_US.utf8 grep -Po '(?<=\<UnityDir\>).*(?=\</UnityDir\>)' "$CREATEPROJECTPATH/Solution.targets"`
+VALHEIMPATH=`LC_ALL=en_US.utf8 grep -Po '(?<=\<GameDir\>).*(?=\</GameDir\>)' "$CREATEPROJECTPATH/Solution.targets"`
+UNITYPATH="$UNITYDIR\Unity.exe"
+
+# convert paths to unix format
+VALHEIMPATH=$(cygpath.exe -u "$VALHEIMPATH")
+UNITYPATH=$(cygpath.exe -u "$UNITYPATH")
+
+
 echo "--------------------------------CALCULATED DATA--------------------------------------------"
 # echo calculated variables for easier script debug
-echo "STANDALONEUNITYPATH =" $STANDALONEUNITYPATH
-echo "UNITYHUBEDITORPATH  =" $UNITYHUBEDITORPATH
 echo "UNITYPATH           =" $UNITYPATH
 echo "CREATEPROJECTPATH   =" $CREATEPROJECTPATH
+echo "VALHEIMPATH         =" $VALHEIMPATH
 echo ""
 echo "-----------------------------PUT VERSION NUMBER--------------------------------------------"
 version=`LC_ALL=en_US.utf8 grep -Po '(?<="version_number": )".*"(?=,)' "$CREATEPROJECTPATH/manifest.json"`
 echo $version
 sed -i "s/\"1.0.0\"/$version/" "$CREATEPROJECTPATH/Plugin/Properties/AssemblyInfo.cs"
 sed -i "s/\"1.0.0\"/$version/" "$CREATEPROJECTPATH/Plugin/Plugin.cs"
+echo ""
+echo "-----------------------COPY ASSEMBLIES TO UNITTY PROJECT-----------------------------------"
+cp "$VALHEIMPATH"/valheim_Data/Managed/assembly_*.dll "$CREATEPROJECTPATH"/Assets/Assemblies -v
 echo ""
 echo "----------------------------GENERATE ASSET BUNDLE------------------------------------------"
 # execute our pipeline script in batch mode of Unity
